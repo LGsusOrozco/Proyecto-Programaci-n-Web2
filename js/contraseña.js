1ac792a84d5122ddeaeb36ffe1d30a41cec294ed
Paula Pérez <69175436+paulaperez14@users.noreply.github.com>
2022-11-04 20:36:07 -0500

    Correcciones ortograficas y enlaces

--- js/contraseña.js
+++ js/contraseña.js
@@ -1,18 +1,18 @@
 function inicioSesion() {
-var usuario = document.getElementById("correo").value;
-var password = document.getElementById("password").value;
+    var usuario = document.getElementById("correo").value;
+    var password = document.getElementById("password").value;
 
-if (usuario== "cliente" && password=="123") {
-    window.location.href = "../vistas/ingresar.html";
-} else if (usuario== "gerente" && password=="123") {
-    window.location.href = "../../vistas/gerente/menuprincipalgerente.html";
-} else if (usuario== "secretaria" && password=="123") {
-    window.location.href = "../../vistas/secretaria_inicio/iniciosecretaria.html";
-} else if (usuario== "profesional" && password=="123") {
-    window.location.href = "interfazprofesionales.html";
-}else if (usuario== "administrador" && password=="123") {
-    window.location.href = "interfazadministrador.html";
-}else {
-    alert("Usuario o contraseña errado");
-}
+    if (usuario== "cliente" && password=="123") {
+        window.location.href = "../cliente/bienvenidacliente.html";
+    } else if (usuario== "gerente" && password=="123") {
+        window.location.href = "../gerente/menuprincipalgerente.html";
+    } else if (usuario== "secretaria" && password=="123") {
+        window.location.href = "../secretaria/iniciosecretaria.html";
+    } else if (usuario== "profesional" && password=="123") {
+        window.location.href = "../profesional/bienvenidaprofesional.html";
+    }else if (usuario== "administrador" && password=="123") {
+        window.location.href = "../administrador/administrador1.html";
+    }else {
+        alert("Usuario o contraseña errado");
+    }
 }
